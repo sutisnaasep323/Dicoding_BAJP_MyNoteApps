593ddb3f1631b3ab9a1f73fd789c9803d2d62e3e
Asep Sutisna <sutisnaasep323@gmail.com>
2022-01-17 17:55:38 +0700

    My Note Apps

---
+++ app/src/main/java/com/example/mynoteapps/helper/NoteDiffCallback.kt
@@ -0,0 +1,28 @@
+package com.example.mynoteapps.helper
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.mynoteapps.database.Note
+
+/*
+NoteDiffCallback untuk melakukan pengecekan apakah ada perubahan list note.
+Kelas ini nanti akan dipanggil di kelas adapter
+ */
+class NoteDiffCallback(private val mOldNoteList: List<Note>, private val mNewNoteList: List<Note>) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return mOldNoteList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return mNewNoteList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return mOldNoteList[oldItemPosition].id == mNewNoteList[newItemPosition].id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldEmployee = mOldNoteList[oldItemPosition]
+        val newEmployee = mNewNoteList[newItemPosition]
+        return oldEmployee.title == newEmployee.title && oldEmployee.description == newEmployee.description
+    }
+}
